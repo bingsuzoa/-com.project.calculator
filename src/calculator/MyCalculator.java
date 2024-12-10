42d97d9af3f25a41cf77328f5b9148961ad729c4
bingsuzoa <zxc_777@naver.com>
2024-12-10 23:39:01 +0900

    test : test코드 작성완료

--- src/calculator/MyCalculator.java
+++ src/calculator/MyCalculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class MyCalculator {
     public double root(String command){
-        int x = Integer.parseInt(command);
+        double x = Double.parseDouble(command);
         return (double) Math.round(Math.sqrt(x) * 100) / 100;
     }
 
